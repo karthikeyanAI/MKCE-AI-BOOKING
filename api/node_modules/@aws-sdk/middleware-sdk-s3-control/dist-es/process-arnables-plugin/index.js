1fa2f4afa8bfb5d909b49509d86b65cbd9c32323
karthikeyanAI <karthiklogu67@gmail.com>
2023-12-19 23:19:14 +0530

    initial commit

---
+++ api/node_modules/@aws-sdk/middleware-sdk-s3-control/dist-es/process-arnables-plugin/index.js
@@ -0,0 +1,4 @@
+export { getProcessArnablesPlugin } from "./getProcessArnablesPlugin";
+export { parseOutpostArnablesMiddleaware, parseOutpostArnablesMiddleawareOptions } from "./parse-outpost-arnables";
+export { updateArnablesRequestMiddleware, updateArnablesRequestMiddlewareOptions } from "./update-arnables-request";
+export { getOutpostEndpoint } from "./getOutpostEndpoint";
---
+++ client/src/Header.jsx
@@ -0,0 +1,34 @@
+import {Link} from "react-router-dom";
+import {useContext} from "react";
+import {UserContext} from "./UserContext.jsx";
+
+export default function Header() {
+  const {user} = useContext(UserContext);
+  return (
+    <header className="flex justify-between">
+          <Link to={'/'} className="flex items-center gap-1">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" dataSlot="icon" className="w-8 h-8">
+                  <path fillRule="evenodd" d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003ZM12 8.25a.75.75 0 0 1 .75.75v3.75a.75.75 0 0 1-1.5 0V9a.75.75 0 0 1 .75-.75Zm0 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" clipRule="evenodd" />
+              </svg>
+
+              <span className="font-bold text-xl">MKCE-AI</span>
+          </Link>
+      
+      <Link to={user?'/account':'/login'} className="flex items-center gap-2 border border-gray-300 rounded-full py-2 px-4 ">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+        </svg>
+        <div className="bg-gray-500 text-white rounded-full border border-gray-500 overflow-hidden">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 relative top-1">
+            <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z" clipRule="evenodd" />
+          </svg>
+        </div>
+        {!!user && (
+          <div>
+            {user.name}
+          </div>
+        )}
+      </Link>
+    </header>
+  );
+}
